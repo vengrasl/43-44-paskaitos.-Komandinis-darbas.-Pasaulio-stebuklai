19d75a419c23ea9296de40932a8b7db93299a02c
InetaKr <ineta.kriunaite@outlook.com>
2023-01-06 14:08:41 +0200

    korteles fixed

--- scripts/main.js
+++ scripts/main.js
@@ -7,45 +7,45 @@ fetch('data.json')
     for (let i = 0; i < 3; i++) {
         const wonder = data.wonders[i];
 
-        const card = document.createElement('div');
-        card.classList.add('card');
+        const card1 = document.createElement('div');
+        card1.classList.add('card1');
 
-        const tourBox = document.createElement('div');
-        tourBox.classList.add('tourBox');
+        const tourBox1 = document.createElement('div');
+        tourBox1.classList.add('tourBox1');
 
         const image = document.createElement('img');
         image.src = wonder.wonderImgUrl;
         image.alt = '';
 
-        tourBox.appendChild(image);
+        tourBox1.appendChild(image);
 
-        const toursInfo = document.createElement('div');
-        toursInfo.classList.add('toursInfo');
+        const toursInfo1 = document.createElement('div');
+        toursInfo1.classList.add('toursInfo1');
 
-        const toursIn1 = document.createElement('div');
-        toursIn1.classList.add('toursIn1');
+        const toursIn12 = document.createElement('div');
+        toursIn12.classList.add('toursIn12');
 
         const Tourtitle = document.createElement('h1');
         Tourtitle.textContent = wonder.title;
 
-        toursIn1.appendChild(Tourtitle);
-        toursInfo.appendChild(toursIn1);
+        toursIn12.appendChild(Tourtitle);
+        toursInfo1.appendChild(toursIn12);
 
-        const toursIn2 = document.createElement('div');
-        toursIn2.classList.add('toursIn2');
+        const toursIn21 = document.createElement('div');
+        toursIn21.classList.add('toursIn21');
 
         const location = document.createElement('p');
         location.textContent = `${wonder.country}, ${wonder.city}`;
 
-        toursIn2.appendChild(location);
+        toursIn21.appendChild(location);
 
         const price = document.createElement('h3');
         price.textContent = `From $${wonder.price}`;
 
-        toursIn2.appendChild(price);
-        toursInfo.appendChild(toursIn2);
-        tourBox.appendChild(toursInfo);
-        card.appendChild(tourBox);
-        container.appendChild(card);
+        toursIn21.appendChild(price);
+        toursInfo1.appendChild(toursIn21);
+        tourBox1.appendChild(toursInfo1);
+        card1.appendChild(tourBox1);
+        container.appendChild(card1);
     }
 });
